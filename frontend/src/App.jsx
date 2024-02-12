1f4b0fe85081ed6423c43e55da7d6723cc0b5773
Viren Khatri <vk102002@gmail.com>
2024-02-12 15:25:14 +0530

    feat: shadcn config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div className="text-3xl font-bold ">Hello World</div>
+      <Button>Heelo</Button>
     </>
   );
 }
