d04a1860954939f557a5a852c5bdaafd01b4b9b3
Kandyyy <kingkandy016@gmail.com>
2024-02-15 19:14:40 +0530

    Added uuid to response model

--- backend/models/Response.js
+++ backend/models/Response.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const responseSchema = new mongoose.Schema({
     user_email: {
@@ -8,6 +9,10 @@ const responseSchema = new mongoose.Schema({
     form_id: {
         type: String,
         required: true
+    },
+    res_id: {
+        type: String,
+        default: uuidv4()
     },
     content: {
         type: mongoose.Schema.Types.Mixed
