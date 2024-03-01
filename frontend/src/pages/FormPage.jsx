d7b0d2925789a13d1071cc8bb2e12e92f9459216
Kandyyy <kingkandy016@gmail.com>
2024-03-01 13:23:50 +0530

    Created Drag and Drop for textbox

--- frontend/src/pages/FormPage.jsx
+++ frontend/src/pages/FormPage.jsx
@@ -1,55 +1,33 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { Card } from "@/components/ui/card";
+import {DndContext, closestCorners, useDroppable} from '@dnd-kit/core';
+import InputField from "@/components/common/InputField";
 
 function List() {
     return (
         <div className="flex flex-col justify-between h-full">
-            <div className="mb-4">
-                <Droppable droppableId="list">
-                    {(provided) => (
-                        <ul className="flex flex-col gap-2 justify-center" {...provided.droppableProps} ref={provided.innerRef}>
-                            <Draggable draggableId="TextField" index={0}>
-                            {(provided) => (
-                                <li ref={provided.innerRef} {...provided.draggableProps}>
-                                    <div className="mt-2" {...provided.dragHandleProps}>Text Box</div>
-                                    <input type="text" placeholder=" " className="mt-2 mx- border-2 border-gray-400 h-12 w-[95%]"/>
-                                </li>
-                            )}
-                            </Draggable>
-                            {provided.placeholder}
-                        </ul>                        
-                    )}
-                </Droppable>
+            <div className="flex flex-col gap-4">
+                <InputField type="text" label="Textbox" inputType="text" _name="txtBox"/>
+                <InputField type="text" label="Textbox" inputType="text" _name="txtBox1"/>
             </div>
         </div>
     );
 }
 
 function Canvas(props){
+    const {setNodeRef} = useDroppable({
+        id: 'canvas',
+      });
     return (
-        <Card className="h-full border border-gray-400 p-2 m-2">        
-            <Droppable droppableId="canvas" direction="vertical">
-                {(provided) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef} className="h-full">
-                        {props.canvasItems.map((field, index) => (
-                            <Draggable key={index} draggableId={field.id} index={index}>
-                            {(provided) => (
-                                <div
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                >
-                                    <div className="m-3">Text Box</div>
-                                    <input type={field.type} placeholder="  Enter text here" className="ml-3 border-2 border-gray-400 h-12 w-[97%]"/>
-                                </div>
-                            )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
+        <Card className="h-full border border-gray-400 p-4" ref={setNodeRef}>        
+            {props.canvasItems.map((item, index) => (
+                <div key={index}>
+                    <label>{item.label}</label>
+                    <input type={item.inputType} name={item._name} />
+                </div>
+            ))}
         </Card>
     )
 }
@@ -58,20 +36,21 @@ function FormPage() {
   const inputOptions = ["Textbox", "Textarea", "Checkbox", "Paragraph", "Dropdown", "Radio Button"];
   const decorationOptions = ["H1 Tag", "H2 Tag", "H3 Tag", "H4 Tag", "H5 Tag", "H6 Tag"];
 
-  // Function to handle drag and drop reorder
-  const onDragEnd = (result) => {
-    console.log(result);
-    setCanvasItems([...canvasItems, {type:"text", id:"1"}])
-  };
-
   const [canvasItems, setCanvasItems] = useState([]);
 
+  function handleDragEnd({over}){
+    console.log(over);
+    if (over && over.id === "canvas") {
+        setCanvasItems([...canvasItems, {id:1, label:"Textbox", inputType:"text", _name:"txtBox"}])
+    }
+  }
+
   return (
-<   DragDropContext onDragEnd={onDragEnd}>
-        <div className="flex justify-center items-center h-screen">
+    <DndContext onDragEnd={handleDragEnd}>
+        <div className="flex justify-center gap-4 items-center h-screen p-4">
 
             {/* Left part */}
-            <div className="w-3/4 h-full mr-4 ml-3">
+            <div className="w-3/4 h-full">
                 <Canvas canvasItems={canvasItems}/>
             </div>
 
@@ -81,7 +60,7 @@ function FormPage() {
             </div>
 
         </div>
-    </DragDropContext>
+    </DndContext>
   );
 }
 
