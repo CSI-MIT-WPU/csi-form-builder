d9e44e070effe411b02b22581777774972256c1a
Kandyyy <kingkandy016@gmail.com>
2024-04-14 00:48:10 +0530

    Major refactoring in pages/FormPage.jsx. Divided file into 2 new components.

--- frontend/src/pages/FormPage.jsx
+++ frontend/src/pages/FormPage.jsx
@@ -3,15 +3,13 @@
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { DndContext, useDroppable, DragOverlay } from "@dnd-kit/core";
 
-import { Card } from "@/components/ui/card";
+import { DndContext } from "@dnd-kit/core";
+
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
 
 import EditDialog from "@/components/FormPage/EditDialog";
 import {
@@ -30,143 +28,11 @@ import {
   ParagraphField,
   SeparatorField,
 } from "@/components/FormPage/ListFields";
-import {
-  CanvasTextField,
-  CanvasCheckBoxField,
-  CanvasDataListField,
-  CanvasFileField,
-  CanvasNumberField,
-  CanvasRadioField,
-  CanvasSelectField,
-  CanvasTextAreaField,
-  CanvasH1Field,
-  CanvasH2Field,
-  CanvasParagraphField,
-  CanvasSeparatorField,
-} from "@/components/FormPage/CanvasFields";
-
-import { MdEdit } from "react-icons/md";
-import { FaTrash } from "react-icons/fa";
+import List from "@/components/FormPage/List";
+import Canvas from "@/components/FormPage/Canvas";
 
 import { setElementData } from "@/utilityFunctions";
 
-function List(props) {
-  return (
-    <div className="flex h-full flex-col">
-      <div className="mb-6 text-center text-3xl font-light">
-        Draggable Fields
-      </div>
-      <Tabs defaultValue="Input" className="w-[100%]">
-        <TabsList className="w-[100%]">
-          <TabsTrigger value="Input">Input</TabsTrigger>
-          <TabsTrigger value="Decoration">Decoration</TabsTrigger>
-        </TabsList>
-        <TabsContent value="Input">
-          <div className="grid-rows-5 justify-items-center gap-y-4 md:grid md:grid-cols-2">
-            <TextField />
-            <EmailField />
-            <NumberField />
-            <TelField />
-            <TextAreaField />
-            <SelectField />
-            <DataListField />
-            <RadioField />
-            <CheckBoxField />
-            <FileField />
-            <DragOverlay>{props.draggedElement}</DragOverlay>
-          </div>
-        </TabsContent>
-        <TabsContent value="Decoration">
-          <div className="grid-rows-5 justify-items-center gap-y-4 md:grid md:grid-cols-2">
-            <H1Field />
-            <H2Field />
-            <ParagraphField />
-            <SeparatorField />
-            <DragOverlay>{props.draggedElement}</DragOverlay>
-          </div>
-        </TabsContent>
-      </Tabs>
-    </div>
-  );
-}
-
-function Canvas(props) {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
-  const [open, setOpen] = useState(false);
-  const [fieldInfo, setFieldInfo] = useState(null);
-
-  function renderElement(element) {
-    const elementMap = {
-      text: <CanvasTextField element={element} />,
-      tel: <CanvasTextField element={element} />,
-      email: <CanvasTextField element={element} />,
-      textarea: <CanvasTextAreaField element={element} />,
-      select: <CanvasSelectField element={element} />,
-      radio: <CanvasRadioField element={element} />,
-      number: <CanvasNumberField element={element} />,
-      file: <CanvasFileField element={element} />,
-      datalist: <CanvasDataListField element={element} />,
-      checkbox: <CanvasCheckBoxField element={element} />,
-      h1: <CanvasH1Field element={element} />,
-      h2: <CanvasH2Field element={element} />,
-      paragraph: <CanvasParagraphField element={element} />,
-      separator: <CanvasSeparatorField element={element} />,
-    };
-    return elementMap[element.type];
-  }
-
-  function handleEdit() {
-    setFieldInfo(props.canvasItems[hoveredIndex]);
-    setOpen(true);
-  }
-
-  function handleDelete() {
-    props.deleteCanvasItems(hoveredIndex);
-  }
-
-  const { setNodeRef } = useDroppable({
-    id: "canvas",
-  });
-  return (
-    <Card className="h-full border border-gray-400 p-4" ref={setNodeRef}>
-      {props.canvasItems.map((item, index) => (
-        <div
-          key={index}
-          className="mb-2 flex w-[100%] flex-col"
-          onMouseOver={() => setHoveredIndex(index)}
-          onMouseOut={() => setHoveredIndex(null)}
-        >
-          <div>
-            <div className="mb-1 flex items-center justify-between">
-              <Label>{item.label}</Label>
-              <div className="flex gap-4">
-                {hoveredIndex === index ? (
-                  <>
-                    <MdEdit onClick={handleEdit} />
-                    <FaTrash onClick={handleDelete} />
-                  </>
-                ) : null}
-              </div>
-            </div>
-            {renderElement(item)}
-          </div>
-        </div>
-      ))}
-      {open ? (
-        <EditDialog
-          canvasItems={props.canvasItems}
-          setCanvasItems={props.setCanvasItems}
-          fieldInfo={fieldInfo}
-          open={open}
-          setOpen={setOpen}
-          setFieldInfo={setFieldInfo}
-          editing={true}
-          hoveredIndex={hoveredIndex}
-        />
-      ) : null}
-    </Card>
-  );
-}
 
 function FormPage() {
   const [open, setOpen] = useState(false);
